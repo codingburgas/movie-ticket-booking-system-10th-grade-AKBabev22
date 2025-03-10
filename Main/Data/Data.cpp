77663552fa4b94838e66c958244f91c765da3928
AKBabev22 <akbabev22@codingburgas.bg>
2025-03-10 15:33:12 +0200

    Add cinema location data

---
+++ Main/Data/Data.cpp
@@ -0,0 +1,14 @@
+#include "Data.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct CinemaData info[] =
+{
+	{"Mall" , 1},
+	{"Center" , 2},
+	{"East" , 3},
+	{"Beach" , 4} 
+};
+
+std::vector<CinemaData> Loc;
