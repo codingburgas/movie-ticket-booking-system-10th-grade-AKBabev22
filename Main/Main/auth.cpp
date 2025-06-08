112afd13272963f25b3e8fcc23b5dcec3dd07dd0
Anton Babev <AKBabev22@codingburgas.bg>
2025-06-09 00:30:43 +0300

    Create registration function

--- Main/Main/auth.cpp
+++ Main/Main/auth.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <filesystem>
 #include "Output.h"
 #include "auth.h"
+using namespace std;
+namespace fs = std::filesystem;
 
 const string USER_FILE = "users.txt";
 
@@ -21,7 +24,52 @@ bool userExists(const string& username) {
 
 void registerUser() {
 
+    string username, password;
+    cout << "=== Register ===" << endl;
+    cout << "Enter username: ";
+    cin >> username;
 
+    if (userExists(username)) {
+        cout << "Username already exists. Try a different one." << endl;
+        return;
+    }
+
+    cout << "Enter password: ";
+    cin >> password;
+
+    // Save to main user file
+    ofstream file(USER_FILE, ios::app);
+    file << username << " " << password << endl;
+    file.close();
+
+    // Create user folder
+    string userFolderPath = BASE_USER_DIR + "\\" + username;
+    try {
+        if (!fs::exists(BASE_USER_DIR)) {
+            fs::create_directory(BASE_USER_DIR);
+        }
+        if (fs::create_directory(userFolderPath)) {
+            string infoFilePath = userFolderPath + "\\Info.txt";
+            ofstream infoFile(infoFilePath);
+            infoFile << "Username: " << username << endl;
+            infoFile << "Password: " << password << endl;
+            infoFile.close();
+            cout << "\nUser directory and Info.txt created successfully." << endl;
+        }
+        else {
+            cerr << "\nFailed to create user folder." << endl;
+        }
+    }
+    catch (const fs::filesystem_error& e) {
+        cerr << "Filesystem error: " << e.what() << endl;
+        return;
+    }
+
+    system("cls");
+    cout << "\nRegistration successful!" << endl;
+
+    // Send to main menu
+    mainMenu(0);
 }
 
 
